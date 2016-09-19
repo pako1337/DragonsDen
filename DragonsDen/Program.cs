783d2ee1954cf2822369c817e796c387e500b9d9
pako1337 <pako1337@gmail.com>
2016-09-19 20:48:05 +0200

    initial dragons den structure

---
+++ DragonsDen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace DragonsDen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var repo = new Repository("c:/users/jporwol/documents/projects/DragonsDen"))
+            {
+                foreach (var log in repo.Commits)
+                {
+                    Console.WriteLine(log.Id);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
