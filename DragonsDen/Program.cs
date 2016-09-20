329a5ec809bbf4b1285af58b5ddbfedd720a4ffd
pako1337 <pako1337@gmail.com>
2016-09-20 17:36:26 +0200

    display frequency of file modifications

--- DragonsDen/Program.cs
+++ DragonsDen/Program.cs
@@ -9,6 +9,8 @@ namespace DragonsDen
 {
     class Program
     {
+        private static Dictionary<string, int> filesFrequency = new Dictionary<string, int>();
+
         static void Main(string[] args)
         {
             using (var repo = new Repository("c:/users/jporwol/documents/projects/DragonsDen"))
@@ -16,8 +18,20 @@ namespace DragonsDen
                 foreach (var log in repo.Commits)
                 {
                     Console.WriteLine(log.Id);
+                    foreach (var file in log.Tree)
+                    {
+                        int frequency = 0;
+                        filesFrequency.TryGetValue(file.Path, out frequency);
+                        frequency++;
+                        filesFrequency[file.Path] = frequency;
+                    }
                 }
             }
+
+            filesFrequency.Select(f => new { Frequency = f.Key, File = f.Value })
+                          .OrderBy(f => f.Frequency)
+                          .ToList()
+                          .ForEach(f => Console.WriteLine($"{f.File}\t{f.Frequency}"));
 
             Console.ReadKey();
         }
