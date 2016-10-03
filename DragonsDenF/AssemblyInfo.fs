8e2ad42a88aeecf879db6c47e1f6d13c923fbf21
pako1337 <pako1337@gmail.com>
2016-10-03 18:52:58 +0200

    f# dragons den

---
+++ DragonsDenF/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace DragonsDenF.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("DragonsDenF")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("DragonsDenF")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("931baa6a-b2e3-4512-909d-fca4070b831e")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ DragonsDenF/Program.fs
@@ -0,0 +1,44 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open LibGit2Sharp
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    use repo = new Repository(argv.[0])
+    let filter = new CommitFilter()
+    filter.SortBy <- CommitSortStrategies.Time
+    let logs = repo.Commits.QueryBy filter
+    logs
+    |> Seq.map  (fun log ->
+        match not (Seq.isEmpty log.Parents) with
+        | true -> "Parent!"
+        | false -> "No parent at all"
+    )
+    |> Seq.iter (fun x -> printf "%s " x)
+
+//         match log with
+//         | true -> Console.WriteLine "parents"
+//         | false -> Console.WriteLine "asdfasdf")
+
+//    System.Console.WriteLine(logs)
+    System.Console.ReadKey() |> ignore
+
+//  if (log.Parents.Any())
+//  {
+//      var oldTree = log.Parents.First().Tree;
+//      var changes = repo.Diff.Compare<TreeChanges>(oldTree, log.Tree);
+//      foreach (var change in changes)
+//      {
+//          UpdateFileFrequency(change.Path);
+//      }
+//  }
+//  else
+//  {
+//      foreach (var file in log.Tree)
+//      {
+//          TreeEntryParsing(file);
+//      }
+//  }
+    0 // return an integer exit code
