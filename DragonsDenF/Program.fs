ea78c3ef83a36f99cf2181aa62f86863cd66360f
pako1337 <pako1337@gmail.com>
2016-10-03 19:21:53 +0200

    return each file as changed from repo

--- DragonsDenF/Program.fs
+++ DragonsDenF/Program.fs
@@ -4,10 +4,14 @@ open LibGit2Sharp
 open System
 
 let getRepoFiles (log:Commit) =
-    "No parent at all"
+    log.Tree
+    |> Seq.map (fun treeEntry ->
+                    match treeEntry.Mode with
+                    | Mode.Directory -> "asdf"
+                    | _ -> "qwe")
 
 let getCommitChangedFiles (log:Commit) =
-    "Parent!"
+    seq { yield "Parent" }
 
 let getChangedFiles (log:Commit) =
     match not (Seq.isEmpty log.Parents) with
@@ -22,30 +26,9 @@ let main argv =
     filter.SortBy <- CommitSortStrategies.Time
     let logs = repo.Commits.QueryBy filter
     logs
-    |> Seq.map  getChangedFiles
+    |> Seq.collect  getChangedFiles
     |> Seq.iter (fun x -> printf "%s " x)
 
-//         match log with
-//         | true -> Console.WriteLine "parents"
-//         | false -> Console.WriteLine "asdfasdf")
-
-//    System.Console.WriteLine(logs)
     System.Console.ReadKey() |> ignore
 
-//  if (log.Parents.Any())
-//  {
-//      var oldTree = log.Parents.First().Tree;
-//      var changes = repo.Diff.Compare<TreeChanges>(oldTree, log.Tree);
-//      foreach (var change in changes)
-//      {
-//          UpdateFileFrequency(change.Path);
-//      }
-//  }
-//  else
-//  {
-//      foreach (var file in log.Tree)
-//      {
-//          TreeEntryParsing(file);
-//      }
-//  }
-    0 // return an integer exit code
+    0
